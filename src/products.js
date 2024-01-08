43dca533f148fda4176654880bdf7c20d36ab00c
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-07 18:12:38 -0700

    added more products need to add images

--- src/products.js
+++ src/products.js
@@ -4,5 +4,47 @@ export const PRODUCTS = [
         productName: 'Iphone',
         price: 999.00,
         productImage:
-    }
+    },
+    {
+        id: 2,
+        productName: 'Supreme Hat',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 3,
+        productName: 'Beanie',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 4,
+        productName: 'Fingerboard',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 5,
+        productName: 'Monitor',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 6,
+        productName: 'Computer',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 7,
+        productName: 'Gloves',
+        price: 999.00,
+        productImage:
+    },
+    {
+        id: 8,
+        productName: 'Bike',
+        price: 999.00,
+        productImage:
+    },
 ]
