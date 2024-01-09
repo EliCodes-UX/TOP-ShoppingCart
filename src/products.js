f603416f88f62013018a95a22077983a2061a32b
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-08 18:39:17 -0700

    added img for shop

--- src/products.js
+++ src/products.js
@@ -3,48 +3,48 @@ export const PRODUCTS = [
         id: 1,
         productName: 'Iphone',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 2,
         productName: 'Supreme Hat',
-        price: 999.00,
-        productImage:
+        price: 99.00,
+        productImage:,
     },
     {
         id: 3,
         productName: 'Beanie',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 4,
         productName: 'Fingerboard',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 5,
         productName: 'Monitor',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 6,
         productName: 'Computer',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 7,
         productName: 'Gloves',
         price: 999.00,
-        productImage:
+        productImage:,
     },
     {
         id: 8,
         productName: 'Bike',
         price: 999.00,
-        productImage:
+        productImage:,
     },
 ]
