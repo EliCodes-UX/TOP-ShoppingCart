001c6a6d9fd12ac77a2897ae1412c596dd3383ce
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-08 19:13:52 -0700

    added img to product class

--- src/products.js
+++ src/products.js
@@ -1,50 +1,59 @@
+import bike from './assets/bike.jpg';
+import monitor from './assets/dell-monitor.jpg';
+import desktop from './assets/desktop.jpg';
+import fingerboard from './assets/fingerboard.jpg';
+import gloves from './assets/gloves.jpg';
+import greenBeanie from './assets/green-beanie.jpg';
+import supreme from './assets/supreme-hat.jpg';
+import iphone from './assets/iphone-15.jpg';
+
 export const PRODUCTS = [
-    {
-        id: 1,
-        productName: 'Iphone',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 2,
-        productName: 'Supreme Hat',
-        price: 99.00,
-        productImage:,
-    },
-    {
-        id: 3,
-        productName: 'Beanie',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 4,
-        productName: 'Fingerboard',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 5,
-        productName: 'Monitor',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 6,
-        productName: 'Computer',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 7,
-        productName: 'Gloves',
-        price: 999.00,
-        productImage:,
-    },
-    {
-        id: 8,
-        productName: 'Bike',
-        price: 999.00,
-        productImage:,
-    },
-]
+  {
+    id: 1,
+    productName: 'Iphone',
+    price: 999.0,
+    productImage: iphone,
+  },
+  {
+    id: 2,
+    productName: 'Supreme Hat',
+    price: 99.0,
+    productImage: supreme,
+  },
+  {
+    id: 3,
+    productName: 'Beanie',
+    price: 15.9,
+    productImage: greenBeanie,
+  },
+  {
+    id: 4,
+    productName: 'Fingerboard',
+    price: 6.9,
+    productImage: fingerboard,
+  },
+  {
+    id: 5,
+    productName: 'Monitor',
+    price: 240.9,
+    productImage: monitor,
+  },
+  {
+    id: 6,
+    productName: 'Desktop',
+    price: 799.9,
+    productImage: desktop,
+  },
+  {
+    id: 7,
+    productName: 'Gloves',
+    price: 23.9,
+    productImage: gloves,
+  },
+  {
+    id: 8,
+    productName: 'Bike',
+    price: 874.9,
+    productImage: bike,
+  },
+];
