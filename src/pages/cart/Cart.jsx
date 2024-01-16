a661c4bcc55d57431d2322df0c574bdd27840176
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-15 21:19:52 -0700

    added all functions to shopping cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,9 +3,13 @@ import ShopContext from '../../context/shop-context';
 import { PRODUCTS } from '../../products';
 import CartItem from './cart-item';
 import './cart.css';
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalAmount } = useContext(ShopContext);
+  const totalAmount = getTotalAmount();
+
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -20,6 +24,15 @@ const Cart = () => {
           }
         })}
       </div>
+      {totalAmount > 0 ? (
+        <div className='checkout'>
+          <p>Subtotal: ${totalAmount}</p>
+          <button onClick={() => navigate('/')}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your Cart is Empty</h1>
+      )}
     </div>
   );
 };
