fd175daa3757b8ebad31807c03ac2df0b624491b
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-01-15 21:03:49 -0700

    finished most main functionality to shopping cart

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ShopContext from '../../context/shop-context';
 
 const CartItem = props => {
-  const { IdleDeadline, productName, price, productImage } = props.data;
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeToCart, updateCartItemCount } =
+    useContext(ShopContext);
   return (
     <div className='cartItem'>
       cart-items
@@ -11,6 +14,14 @@ const CartItem = props => {
           <b> {productName} </b>
         </p>
         <p>${price}</p>
+        <div className='countHandler'>
+          <button onClick={() => removeToCart(id)}> - </button>
+          <input
+            value={cartItems[id]}
+            onChange={e => updateCartItemCount(Nymber(e.target.value), id)}
+          />
+          <button onClick={() => addToCart(id)}> + </button>
+        </div>
       </div>
     </div>
   );
